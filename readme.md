f792052902ccca29ae30b462df2226595723bf80
Jelle Versele <jelle.versele@internetarchitects.be>
2015-07-03 11:58:24 +0200

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,11 @@
+#Flux demo
+
+Flux demo app written in ES6 using webpack. Work in progress ...
+
+## running the app
+```
+npm install
+npm run dev
+```
+
+Then navigate to http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    entry: {
+        'js/main.js': [
+            './src/index',
+            'webpack/hot/only-dev-server'
+        ]
+    },
+    output: {
+        path: './static/assets/',
+        publicPath: '/assets/',
+        filename: '[name]'
+    },
+    module: {
+        loaders: [
+            { test: /\.js(x?)$/, exclude: /node_modules/, loader: require.resolve('babel-loader') },
+            { test: /\.json$/, loader: 'json-loader'}
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './static',
+        inline: true
+    }
+};
